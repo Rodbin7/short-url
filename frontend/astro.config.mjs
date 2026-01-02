45b4406fcb54c7efe772989961a6f2413c51abd7
Rodbin7 <dorantejosue345@gmail.com>
2026-01-02 09:38:42 -0400

    Configurar adaptador de Netlify

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,21 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
+
+
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
     server: {
       proxy: {
-        // Redirige las llamadas a la API al backend
         '/api': {
           target: 'http://localhost:5000',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '/api'),
-          ws: true,
+          ws: false,
         },
-        // Redirige los enlaces cortos al backend
         '/r/': {
           target: 'http://localhost:5000',
           changeOrigin: true,
